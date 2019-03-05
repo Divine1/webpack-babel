b8e1f2e2a90781127ee36ac282d966611f0b4679
divine <cdivine304@gmail.com>
2019-03-06 00:51:38 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import button from './button';
+
+console.log("in index.js --save hey")
+
+var aaa = ['a' , 'b' , 'c' , 'd ' ];
+
+aaa.forEach( (element,index ) => {
+    console.log("element ", element)
+});
+
+button()
